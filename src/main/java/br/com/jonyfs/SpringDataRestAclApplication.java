7eab1757b8a25774ed0f9b32bf5a33f8f4602f54
Jony Santos <jonyferreira@hotmail.com>
2016-12-13 20:26:02 -0200

    initial commit

---
+++ src/main/java/br/com/jonyfs/SpringDataRestAclApplication.java
@@ -0,0 +1,12 @@
+package br.com.jonyfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestAclApplication.class, args);
+	}
+}
